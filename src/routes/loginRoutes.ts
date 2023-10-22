267901867affec2351bfd42e123f669987f2a4a5
0mgABear <yeoyi94@gmail.com>
2023-10-23 00:32:39 +0800

    section14 halfway mark

---
+++ src/routes/loginRoutes.ts
@@ -0,0 +1,29 @@
+//not exporting a class, hence start with lower case
+import { Router, Request, Response } from "express";
+import bodyParser from "body-parser";
+
+const router = Router();
+router.get("/login", (req: Request, res: Response) => {
+  res.send(
+    `
+    <form method="POST">
+      <div>
+        <label>Email </label>
+        <input name="email"/>
+      </div>
+      <div>
+        <label>Password </label>
+        <input name="password" type="password"/>
+      </div>
+      <button>Submit</button>
+    </form>
+    `
+  );
+});
+
+router.post("/login", (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  res.send(email + password);
+});
+
+export { router };
